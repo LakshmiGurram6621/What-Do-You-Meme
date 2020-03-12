183ab003fdd596a41da19639ec76bb155da776a4
Lakshmi Gurram <lakshmi@client142-161.wireless.newpaltz.edu>
2020-03-12 15:01:45 -0400

    committinh chages

--- server/controllers/game.js
+++ server/controllers/game.js
@@ -3,9 +3,13 @@ const router =express.Router()
 const quoteCards=require("../models/quoteCards")
 
 router
-    .get('/quoteCards',(req,res) => res.send(quoteCards))
-    .post('/quoteCards',(req,res)) => {
-         quoteCards.add(req.query.text)
+     .use('/quoteCards',(req,res)=>{
+         console.log({method:req.method,body:req.body});
+         next();
+     })
+    .get('/quoteCards',(req,res,next) => console.log({method:req.method,query:req.query}))
+    .post('/quoteCards',(req,res) => {
+         quoteCards.add(req.body.text)
          res.send(quoteCards.list.length)
     })
 module.exports=router;    
--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 console.log("Hello world");
 const express = require('express')
+const router = express.Router();
 const gameController =require('./controllers/game');
 
 const app = express()
 const port = 3000
 
 app
+.use(express.json())
+.use(express.urlencoded({extended:true}))
 .get('/', (req, res) => res.send('Hello World!'))
 .use('/game',gameController)
 
